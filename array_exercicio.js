7bb8e5be19e2da711dfd22677674759f6c70428f
predasoli-SVG <predasolienzo@gmail.com>
2026-09-25 11:27:54 -0300

    renovado1

---
+++ array_exercicio.js
@@ -0,0 +1,25 @@
+//ex1 
+let cores= ["azul", "preto", "vermelho"]
+console.log(cores)
+console.log()
+
+let numeros = [1,2,3,4,5]
+console.log(numeros[0])
+console.log(numeros[4])
+console.log()
+
+let frutas = ["banana", "maça"]
+frutas.push("mamão")
+console.log(frutas)
+console.log()
+
+let nomes = ["Ana", "Bruno", "Carlos", "Diana"];
+let nomeRemovido = nomes.pop();
+console.log(nomes); 
+console.log("Nome removido:", nomeRemovido);
+console.log()
+
+
+let times = ["Flamengo", "Palmeiras", "São Paulo", "Real Madrid"];
+let quantidade = times.length;
+console.log("O número de elementos no array é:", quantidade); 
